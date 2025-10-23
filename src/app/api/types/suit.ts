e86b3ac271a60cad1b2b23f16d229d5890845d79
Adrian Teske <adrian.teske@icloud.com>
2025-10-23 11:17:54 +0200

    added cards assets and player hand + card component

---
+++ src/app/api/types/suit.ts
@@ -0,0 +1,6 @@
+export enum Suit {
+  Hearts = 'hearts',
+  Diamonds = 'diamonds',
+  Clubs = 'clubs',
+  Spades = 'spades',
+}
--- src/app/components/playing-field/playing-field.component.ts
+++ src/app/components/playing-field/playing-field.component.ts
@@ -1,16 +1,25 @@
 import { Component, inject } from '@angular/core';
 import { SupabaseClient } from '@supabase/supabase-js';
 import { SupabaseService } from '../../services/supabase.service';
+import { PlayerHandComponent } from "../player-hand/player-hand.component";
+import { Suit } from '../../api/types/suit';
+import { Rank } from '../../api/types/rank';
 
 @Component({
   selector: 'app-playing-field',
   standalone: true,
-  imports: [],
+  imports: [PlayerHandComponent],
   templateUrl: './playing-field.component.html',
   styleUrl: './playing-field.component.scss',
 })
 export class PlayingFieldComponent {
   supabaseService = inject(SupabaseService);
+
+  cards = [
+    { suit: Suit.Hearts, rank: Rank.Ace },
+    { suit: Suit.Spades, rank: Rank.King },
+    { suit: Suit.Diamonds, rank: Rank.Ten },
+  ];
 
   async btnCreateGameClick() {
     // this.supabaseService.getPlayers().then((response) => {
